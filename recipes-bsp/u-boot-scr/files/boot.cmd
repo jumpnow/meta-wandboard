f4ff3588f203458d735ca1899457ca84e60694ed
Scott Ellis <scott@jumpnowtek.com>
2022-08-27 16:25:12 -0400

    Fix bootloader for revb1 boards

--- recipes-bsp/u-boot-scr/files/boot.cmd
+++
@@ -1,9 +0,0 @@
-setenv bootpart 0:1
-setenv bootdir /boot
-setenv mmcroot /dev/mmcblk2p1 ro
-setenv mmcrootfstype ext4 rootwait
-setenv bootargs console=${console} root=${mmcroot} rootfstype=${mmcrootfstype}
-run findfdt
-load mmc ${bootpart} ${fdt_addr} ${bootdir}/${fdtfile}
-load mmc ${bootpart} ${loadaddr} ${bootdir}/zImage
-bootz ${loadaddr} - ${fdt_addr}
---
+++ recipes-bsp/u-boot/u-boot_2022.01.bbappend
@@ -0,0 +1,3 @@
+FILESEXTRAPATHS:prepend := "${THISDIR}/files:"
+
+SRC_URI += "file://0001-Boot-a-wandboard-revb1-board.patch"
